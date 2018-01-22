712e6a3194f11d814879bbd102f940e3311e6683
gkurl <gkurl@hotmail.co.uk>
2018-01-22 16:50:30 +0000

    first wave of changes initial layouts ordering

--- public/products.php
+++ public/products.php
@@ -1,1 +1,12 @@
 <?php
+// set the page title
+$page_title = "Products";
+
+//page header include
+include 'layout_head.php';
+
+//main content goes here
+
+// include footer
+
+include 'layout_footer.php';
